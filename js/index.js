c2704d37d786db242bedbb97209ab5ef673ce63b
foxychmoxy <foxxychmoxy@gmail.com>
2019-12-29 22:42:44 +0600

    Added Home Section and Why We Section

---
+++ js/index.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    // nav item click
+    $(".nav-item").click(function(){
+        var elements = $(".nav-item");
+        for(element of elements){
+            element.classList.remove("active");
+        }
+        $(this).addClass("active");
+    })
+
+    // nav link anchor
+    function scrollToAnchor(aid){
+		var tag = $(`#${aid}`);
+		$('html,body').animate({scrollTop: tag.offset().top}, 'slow');
+	}
+	
+	$(".nav-link").click(function() {
+		scrollToAnchor($(this).attr("data-anchor"));
+    });
+
+    $(".navbar-brand").click(function(){
+        $('html,body').animate({scrollTop: $('#home').offset().top}, 'slow');
+    });
+
+    // scroll navbar
+    var color = 'bg-light';
+
+    function navScroll(color){
+        if($(document).scrollTop() > 20){
+            $(".navbar").addClass(color);
+        } if($(document).scrollTop() < 20){
+            $(".navbar").removeClass(color);
+        }
+    }
+
+    $( window ).scroll(function() {
+        navScroll(color);
+    });
+
+    navScroll(color);
+});
